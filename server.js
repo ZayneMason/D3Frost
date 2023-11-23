6a0ce403256ffa8bf61c28988251bbf8c1620b63
ZayneMason <83897407+ZayneMason@users.noreply.github.com>
2023-11-22 21:39:20 -0600

    Changed file structure
    Removed folder for API as I will alter package.json to run React app and API concurrently.

---
+++ server.js
@@ -0,0 +1,104 @@
+// Express setup
+let express = require('express');
+var app = express();
+let router = express.Router();
+app.use(express.json());
+app.use('/api/', router);
+
+// Import Snowflake SDK and define functions for creating connections and connection pools
+var snowflake = require('snowflake-sdk');
+
+function createSnowflakeConnection(account, accessUrl, username, password) {
+  return snowflake.createConnection({
+    account: account,
+    accessUrl: accessUrl,
+    username: username,
+    password: password,
+  });
+}
+
+function createSnowflakeConnectionPool(account, accessUrl, username, password) {
+  return snowflake.createPool( 
+    {
+      account: account,
+      accessUrl: accessUrl,
+      username: username,
+      password: password,
+    }, 
+    { 
+      min: 1,
+      max: 2,
+    });
+}
+
+async function testConnection() { await snowflakeConnection.isValidAsync(); }
+
+// POST to connect to Snowflake
+router.post('/snowflake-connect', function(req, res, next) {
+  console.log(req.body);
+  const snowflakeConnection = createSnowflakeConnection(req.body.account, req.body.accessUrl, req.body.username, req.body.password);
+  snowflakeConnection.connect(function(err, conn) {
+    console.log('Connecting to Snowflake...')
+    if (err) {
+        res.send('Unable to connect: ' + err.message);
+    } else {
+        let connectionID = conn.getId();
+        res.send('Successfully connected to Snowflake. Connection ID: ' + connectionID + '.');
+    }
+  });
+});
+
+// POST to test Snowflake connection
+router.post('/snowflake-test', function(req, res, next) {
+  if (testConnection()) {
+    res.send('Connection is valid!');
+  }
+  else {
+    res.send('Connection is not valid!');
+  }
+});
+
+// POST to get list of databases/schemas in Snowflake
+router.post('/snowflake-schema', function(req, res, next) {
+  const snowflakeConnectionPool = createSnowflakeConnectionPool(req.body.account, req.body.accessUrl, req.body.username, req.body.password);
+  snowflakeConnectionPool.use(async (clientConnection) =>
+  {
+    const statement = await clientConnection.execute({
+        sqlText: 'show terse schemas;',
+        complete: function (err, stmt, rows)
+        {
+            resultRows = '';
+            if (err)
+            {
+                res.send('Failed to execute statement due to the following error: ' + err.message);
+            }
+            res.send(rows);
+        }
+    });
+  });
+  }
+);
+
+// POST to send a query to Snowflake
+router.post('/snowflake-query', function(req, res, next) {
+  const snowflakeConnectionPool = createSnowflakeConnectionPool(req.body.account, req.body.accessUrl, req.body.username, req.body.password);
+  snowflakeConnectionPool.use(async (clientConnection) =>
+{
+    const statement = await clientConnection.execute({
+        sqlText: 'select * from ' + req.body.database + '.' + req.body.schema + '.' + req.body.table + ';',
+        complete: function (err, stmt, rows)
+        {
+            resultRows = '';
+            if (err)
+            {
+                res.send('Failed to execute statement due to the following error: ' + err.message);
+            }
+            res.send(rows);
+        }
+    });
+  });
+});
+
+var server = app.listen(5000, function() {
+  console.log('Node server is running on http://localhost:5000..');
+});
